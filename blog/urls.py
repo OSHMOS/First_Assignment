ab358dabc86064d49fc2bbcae52fa2a65a537646
oshmos <ojames9707@gmail.com>
2022-05-07 19:07:21 +0900

    First Assignment

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
     path('detail/<int:post_id>/', views.detail, name='detail'),
     path('create/', views.create, name='create'),
     path('update/<int:post_id>/', views.update, name='update'),
